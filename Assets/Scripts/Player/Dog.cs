beba175304d2f4e25da36cde0119c75a79c7a15d
현지 <h91789084@gmail.com>
2024-11-06 11:07:28 +0900

    아군프리팹이랑 버튼 연결완료~그리고 돈도차감

--- Assets/Scripts/Player/Dog.cs
+++ Assets/Scripts/Player/Dog.cs
@@ -1,39 +1,42 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Dog : MonoBehaviour
 {
-    public float playerMoveSpeed; // 이동 속도
-    public int attackSpeed; // 공격 속도
-    public float attackRange; // 공격 범위
-    public int attackPower; // 공격력
-    public int health; // 체력
+    public float playerMoveSpeed;
+    public int attackSpeed;
+    public float attackRange;
+    public int attackPower;
+    public int health;
 
-    public Vector3 spawnPlayerPosition; // 스폰 위치
-    public float playerTargetX; // 목표 x 위치
+    public Vector3 spawnPlayerPosition;
+    public float playerTargetX;
+
+    private void Start()
+    {
+        // 시작할 때 자동으로 Initialize 호출
+        Initialize(transform.position);
+    }
 
     public void Initialize(Vector3 spawnPos)
     {
         spawnPlayerPosition = spawnPos;
         transform.position = spawnPlayerPosition;
 
-        // 목표 위치 설정 (x축으로만 50 단위 이동)
-        playerTargetX = spawnPlayerPosition.x + 100f;
+        // 현재 위치에서 왼쪽으로 100 이동하도록 설정
+        playerTargetX = spawnPlayerPosition.x - 100f;
     }
 
     private void Update()
     {
-        MoveTowardsPlayerTarget(); // 매 프레임마다 이동
+        MoveTowardsPlayerTarget();
     }
 
     private void MoveTowardsPlayerTarget()
     {
-        // 현재 위치에서 목표 위치로 이동
-        if (transform.position.x < playerTargetX)
+        // 현재 위치가 목표보다 크면(오른쪽에 있으면) 왼쪽으로 이동
+        if (transform.position.x > playerTargetX)
         {
-            transform.position += new Vector3(playerMoveSpeed * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(-playerMoveSpeed * Time.deltaTime, 0, 0);
         }
     }
 }
